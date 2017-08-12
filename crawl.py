66d71331002ddbd2c4811a0740fcbef3fe182cb7
aditisakhare <aditisakhare21@gmail.com>
2017-08-12 14:42:32 +0530

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -14,6 +14,8 @@ def get_page(url):
 
 def get_next_target(page):
 	start_link = page.find('<a href=')
+	if start_link == -1: 
+        	return None, 0
 	start_quote = page.find('"', start_link)
 	end_quote = page.find('"', start_quote + 1)
 	url = page[start_quote + 1:end_quote]
